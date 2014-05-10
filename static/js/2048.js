30ed4f5fbf86795df484616841589fce6c781f54
qiangtou <qiangtou@gmail.com>
2014-05-11 01:30:52 +0800

    add move check

--- static/js/2048.js
+++ static/js/2048.js
@@ -1,141 +1,162 @@
 (function(){
 	console = console || {};
-        console.log = console.log || function () {}
+	console.log = console.log || function () {}
 
-        var transform = {
-            right: function (arr) {
-                var a = [];
-                var col = arr[0].length,
-                    row = arr.length;
-                for (var i = 0; i < col; i++) {
-                    a[i] = a[i] || []
-                    for (var j = 0; j < row; j++) {
-                        a[i][row - 1 - j] = arr[j][i]
-                    }
-                }
-                return a;
-            },
-            left: function (arr) {
-                var a = [];
-                var col = arr[0].length,
-                    row = arr.length;
-                for (var i = 0; i < col; i++) {
-                    for (var j = 0; j < row; j++) {
-                        a[col - 1 - i] = a[col - 1 - i] || [];
-                        a[col - 1 - i][j] = arr[j][i]
-                    }
-                }
-                return a;
-            }
-
-        }
-        var move2Left = function (a) {
-            var _a = [],
-                empNum = [],
-                move = false;
-            var _a = a.map(function (list) {
-                var _list = list.slice();
-                var e1, e2, emp = [],
-                    r = [];
-                for (var i = 0, len = list.length; i < len; i++) {
-                    e1 = list[i];
-                    if (e1 == 0) {
-                        emp.push(e1);
-                        continue;
-                    }
-                    r.push(e1);
-                    move = move || (list[i - 1] === 0)
-                    do {
-                        e2 = list[++i];
-                        if (e2 > 0) {
-                            if (e1 == e2) {
-                                move = true;
-                                emp.push(list[i] = 0);
-                                game.score += (r[r.length - 1] = e1 * 2);
-                            } else {
-                                i--;
-                            }
-                            break;
-                        } else if (e2 == 0) {
-                            emp.push(e2);
-                        }
-                    } while (i < len);
-                }
-                console.log(_list, '-->', r, emp)
-                empNum.push((empNum[empNum.length - 1] || 0) + emp.length);
-                return r.concat(emp);
-            });
-
-            if (move) {
-                var j, n, m, sum = empNum[empNum.length - 1];
-                var r = j = Math.random() * sum | 0;
-                for (var i = 0, len = empNum.length - 1; i < len; i++) {
-                    n = empNum[i];
-                    m = empNum[i + 1]
-                    if (r < n) {
-                        break;
-                    }
-                    if (r < m) {
-                        i++;
-                        j = r - n;
-                        break;
-                    }
-                }
-                var nn = [2, 2, 2, 2, 2, 2, 2, 2, 4]
-                nn = nn[Math.random() * nn.length | 0]
-                var xx = i;
-                var yy = _a[i].length - j - 1;
-                console.log('移动后[' + xx + '][' + yy + ']的值是' + _a[xx][yy], '放入', nn)
-                _a[xx][yy] = nn;
-				game.addHistory();
-            }
-            return _a;
-        }
-
-        var initdata = function (num) {
-            var arr = [
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-            ];
-            while (num--) {
-                var x = Math.random() * 4 | 0;
-                var y = Math.random() * 4 | 0;
-                if (arr[x][y] > 0) num++;
-                arr[x][y] = 2;
-            }
-            game.h = [arr]
-            return arr;
-        }
-
-        var game = avalon.define('2048', function (vm) {
-            vm.score = 0;
-            vm.arr = [];
-            vm.h = [];
-            vm.key = '';
-            vm.hu = function () {
-                if (vm.h.length > 1) {
-                    vm.h.shift();
-                    vm.arr = vm.h[0];
-                }
-            }
-            vm.restart = function () {
-            	vm.score=0;
-                vm.arr = initdata(2);
-            }
-			vm.addHistory=function(){
-                vm.h.unshift(clone(game.arr));
+	var transform = {
+		right: function (arr) {
+			var a = [];
+			var col = arr[0].length,
+			row = arr.length;
+			for (var i = 0; i < col; i++) {
+				a[i] = a[i] || []
+				for (var j = 0; j < row; j++) {
+					a[i][row - 1 - j] = arr[j][i]
+				}
 			}
-            vm.check = function () {
+			return a;
+		},
+		left: function (arr) {
+			var a = [];
+			var col = arr[0].length,
+			row = arr.length;
+			for (var i = 0; i < col; i++) {
+				for (var j = 0; j < row; j++) {
+					a[col - 1 - i] = a[col - 1 - i] || [];
+					a[col - 1 - i][j] = arr[j][i]
+				}
+			}
+			return a;
+		}
 
-            }
-            vm.move = function (key) {
-                console.log(key)
-                vm.key = key;
-                vm.arr = action[key](game.arr)
-            }
-        });
+	}
+	var move2Left = function (a) {
+		var _a = [],i,j,k,x,y,
+		empNum = [],
+		move = false;
+		var _a = a.map(function (list) {
+			var _list = list.slice();
+			var e1, e2, emp = [],
+			r = [];
+			for (i = 0, len = list.length; i < len; i++) {
+				e1 = list[i];
+				if (e1 == 0) {
+					emp.push(e1);
+					continue;
+				}
+				r.push(e1);
+				move = move || (list[i - 1] === 0)
+				do {
+					e2 = list[++i];
+					if (e2 > 0) {
+						if (e1 == e2) {
+							move = true;
+							emp.push(list[i] = 0);
+							game.score += (r[r.length - 1] = e1 * 2);
+						} else {
+							i--;
+						}
+						break;
+					} else if (e2 == 0) {
+						emp.push(e2);
+					}
+				} while (i < len);
+			}
+			console.log(_list, '-->', r, emp)
+			empNum.push((empNum[empNum.length - 1] || 0) + emp.length);
+			return r.concat(emp);
+		});
+
+		//0的个数
+		var sum = empNum[empNum.length - 1];
+		if (game.moved=move) {
+			var n, m, r = j = Math.random() * sum | 0;
+			for (i = 0, len = empNum.length - 1; i < len; i++) {
+				n = empNum[i];
+				m = empNum[i + 1]
+				if (r < n)break;
+				if (r < m) {
+					i++;
+					j = r - n;
+					break;
+				}
+			}
+			var newNum = [2, 2, 2, 2, 2, 2, 2, 2, 4]
+			newNum = newNum[Math.random() * newNum.length | 0]
+			x = i;
+			y = _a[i].length - j - 1;
+			console.log('移动后[' + x + '][' + y + ']的值是' + _a[x][y], '放入', newNum)
+			_a[x][y] = newNum;
+		}
+		var canmove=true;
+		if(sum<2){
+			//当格子满时检测是否可移动，做24次检测
+			var next;
+			canmove=false
+			for(i=2;i--;)
+				for(j=4;j--;)
+					for(k=3;k--;){
+						x=i?j:k;
+						y=i?k:j;
+						next=i?_a[x][y+1]:_a[x+1][y];
+						if(_a[x][y]==next){
+							canmove=true;
+							break;
+						}
+					}
+		}
+		game.canmove=canmove;
+		return _a;
+	}
+
+	var initdata = function (num) {
+		var arr=[
+			[0,0,0,0],
+			[0,0,0,0],
+			[0,0,0,0],
+			[0,0,0,0]
+		];
+		while (num--) {
+			var x = Math.random() * 4 | 0,
+			y = Math.random() * 4 | 0;
+			if (arr[x][y] > 0) num++;
+			arr[x][y] = 2;
+		}
+		return arr;
+	}
+
+	var game = avalon.define('2048', function (vm) {
+		vm.score = 0;
+		vm.arr = [];
+		vm.history = [];
+		vm.key = '';
+		vm.moved=false;
+		vm.canmove=true;
+		vm.regret = function () {
+			if (vm.history.length > 1) {
+				vm.history.shift();
+				vm.arr = vm.history[0];
+			}
+		}
+		vm.restart = function () {
+			vm.score=0;
+			vm.canmove=true;
+			var arr=initdata(2);
+			vm.history=[arr];
+			vm.arr = arr;
+		}
+		vm.move = function (key) {
+			console.log(key)
+			vm.key = key;
+			var arr=action[key](game.arr);
+			if(vm.moved){
+				vm.arr=arr;
+				vm.history.unshift(arr);
+			}
+			if(!vm.canmove){
+				alert('Game Over !');
+			}
+		}
+	});
         game.restart();
         var keymap = {
             37: 'left',
@@ -166,17 +187,7 @@
                 arr = move2Left(arr);
                 arr = transform.left(arr);
                 return arr;
-            },
-            other: function () {
-                console.log('hehe')
             }
-        }
-        var clone = function (arr) {
-            return arr.map(function (list) {
-                return list.map(function (e) {
-                    return e;
-                });
-            });
         }
         document.body.addEventListener('keydown', function (e) {
             var keycode = e.keyCode | e.which,
