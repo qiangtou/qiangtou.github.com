a8d2843ec2401a28e52b6cdd1d1d5f5712aaf4ad
qiangtou <qiangtou@gmail.com>
2014-05-11 14:19:11 +0800

    add background color

--- static/js/2048.js
+++ static/js/2048.js
@@ -131,6 +131,24 @@
 		vm.key = '';
 		vm.moved=false;
 		vm.canmove=true;
+		vm.$colors=[
+			'#ffffff',
+			'#eee4da',
+			'#ede0c8',
+			'#f2b179',
+			'#f59563',
+			'#f67c5f',
+			'#f65e3b',
+			'#edcf72',
+			'#edcc61',
+			'#edc850',
+			'#edc53f',
+			'#edc22e',
+			'#3c3a32'
+		]	
+		vm.getColor=function(n){
+			return vm.$colors[Math.log(n)/Math.log(2)|0]; 
+		}
 		vm.regret = function () {
 			if (vm.history.length > 1) {
 				vm.history.shift();
